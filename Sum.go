3865d9777d3e9c967879d1d1f87e84870e202454
aravinds13 <aravindtvm13@gmail.com>
2019-05-26 00:45:14 +0530

    Added Sum.go

---
+++ Sum.go
@@ -0,0 +1,16 @@
+//add two numbers
+//also explores different ways to declare a variable
+
+package main
+import "fmt"
+
+func main(){
+  var a int
+  a=5
+  var(
+    b=6
+    c=10
+  )
+  d:=a+b+c
+  fmt.Println("Sum is ",d);
+}
